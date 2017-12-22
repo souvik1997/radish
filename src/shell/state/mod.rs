7daf53c87733962abbe74aa1c2a4a1023f3e89a7
Souvik Banerjee <souvik1997@gmail.com>
2017-12-22 17:15:24 -0600

    Add colored text support

--- src/shell/state/mod.rs
+++ src/shell/state/mod.rs
@@ -3,6 +3,8 @@ use super::jobs::Job;
 use std::env;
 use super::super::sys;
 extern crate users;
+extern crate ansi_term;
+use self::ansi_term::Colour;
 
 
 pub struct ShellState {
@@ -31,6 +33,9 @@ impl readline::delegate::Delegate for ShellState {
         };
         let username = users::get_current_username().unwrap_or(String::from("(none)"));
         let last_character = if users::get_current_uid() == 0 { "#" } else { "$" };
-        String::from(format!("{username}@{cwd}{last_character} ", username=username, cwd=cwd, last_character=last_character))
+        String::from(format!("{username}@{cwd}{last_character} ",
+                             username=Colour::Red.normal().paint(username).to_string(),
+                             cwd=Colour::Green.normal().paint(cwd).to_string(),
+                             last_character=last_character))
     }
 }
